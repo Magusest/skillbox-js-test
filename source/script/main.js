5d9ea4b2538de1aef095de5d03570eec6afa1ce9
Magusest <redikor49@gmail.com>
2024-03-20 17:25:27 +0300

    Add mobile menu logic

--- source/script/main.js
+++ source/script/main.js
@@ -1,11 +1,12 @@
 import "../scss/style.scss"
 import "./utils/prism.js";
 import {iosVhFix} from "./utils/ios-vh-fix.js";
+import initMenuButton from "./modules/header/init-page-menu";
 
 window.addEventListener('DOMContentLoaded', () => {
   iosVhFix();
 
   window.addEventListener('load', () => {
-
+    initMenuButton();
   });
 });
---
+++ source/script/modules/header/init-page-menu.js
@@ -0,0 +1,57 @@
+const body = document.querySelector('body');
+const menuButton = body.querySelector('[data-sandwich="data-sandwich"]');
+const nav = body.querySelector('[data-main-nav="nav"]');
+const navItems = nav.querySelectorAll('[data-nav-item="item"]')
+const logo = body.querySelector('[data-header-logo="data-header-logo"]');
+
+const itemApearTime = 100;
+const breackpoint = 1024;
+
+
+export default function initMenuButton() {
+    if (!menuButton) {
+        return
+    }
+
+    const observer = new ResizeObserver(entries => {
+        const bodyWidth = entries[0].contentRect.width;
+        if (bodyWidth >= breackpoint) {
+            closeMenuHandler();
+        }
+    })
+
+    const openMenuHandler = () => {
+        body.classList.toggle('scroll-lock');
+        menuButton.classList.toggle('is-active');
+        nav.classList.toggle('is-active');
+        logo.classList.toggle('is-menu');
+        
+        navItems.forEach((item, key) => {
+            setTimeout(() => {
+                item.classList.toggle('is-active')
+            }, key  * itemApearTime + 100);             
+        })
+    }
+
+    const closeMenuHandler = () => {
+        body.classList.remove('scroll-lock');
+        nav.classList.remove('is-active');
+        menuButton.classList.remove('is-active');
+        logo.classList.remove('is-menu');    
+
+        navItems.forEach((item) => {
+            item.classList.remove('is-active')
+        })    
+    }
+
+    const escapeHandler = (evt) => {
+        if (evt.key === 'Escape') {
+            closeMenuHandler()
+        }
+    }
+
+
+    menuButton.addEventListener('click', openMenuHandler)
+    document.addEventListener('keydown', escapeHandler)
+    observer.observe(body);
+}
